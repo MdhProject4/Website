d613a631cb404aacf5272400e2b6def50a41ab81
kraxarn <emil202821@hotmail.com>
2018-10-10 21:53:24 +0200

    Fix some warnings

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace ProjectFlight.Controllers
 {
-    public class HomeController : Controller
+	public class HomeController : Controller
     {
         public IActionResult Index()
         {
