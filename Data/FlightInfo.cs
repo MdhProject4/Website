9ab22440b8d3b9c74337f296451dd154cd782ca5
kraxarn <emil202821@hotmail.com>
2018-10-12 15:38:33 +0200

    Initial flight info classes

---
+++ Data/FlightInfo.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using ProjectFlight.Enums;
+
+namespace ProjectFlight.Data
+{
+	/// <summary>
+	/// A more user friendly and descriptive version of <see cref="FlightInfoResponse"/>
+	/// </summary>
+	public class FlightInfo
+	{
+		/// <summary>
+		/// Identifier broadcast by the aircraft
+		/// </summary>
+		public readonly string Identifier;
+
+		/// <summary>
+		/// Aircraft registration number
+		/// </summary>
+		public readonly string RegistrationNumber;
+
+		/// <summary>
+		/// First seen
+		/// </summary>
+		public readonly DateTime FirstSeen;
+
+		/// <summary>
+		/// Time the aircraft has been tracked for
+		/// </summary>
+		public readonly TimeSpan Tracked;
+
+		/// <summary>
+		/// Latitude over the ground
+		/// </summary>
+		public readonly float Latitude;
+
+		/// <summary>
+		/// Longitude over the ground
+		/// </summary>
+		public readonly float Longitude;
+
+		/// <summary>
+		/// Time position was last reported
+		/// </summary>
+		public readonly DateTime LastUpdate;
+
+		/// <summary>
+		/// Speed in knots
+		/// </summary>
+		// TODO: Convert this to something useful
+		public readonly float Speed;
+
+		/// <summary>
+		/// Speed type
+		/// </summary>
+		public readonly ESpeedType SpeedType;
+
+		/// <summary>
+		/// Angle clockwise in degrees
+		/// </summary>
+		public readonly float Angle;
+
+		/// <summary>
+		/// Aircraft's model
+		/// </summary>
+		public readonly string Model;
+
+		/// <summary>
+		/// Description of aircraft's model
+		/// </summary>
+		public readonly string ModelDescription;
+
+		/// <summary>
+		/// Manufacturer's name
+		/// </summary>
+		public readonly string Manufacturer;
+
+		/// <summary>
+		/// Year manufactured
+		/// </summary>
+		public readonly int Year;
+
+		/// <summary>
+		/// Name of aircraft's operator
+		/// </summary>
+		public readonly string Operator;
+
+		/// <summary>
+		/// Vertical speed in feet per minute
+		/// </summary>
+		// TODO: Convert this to something useful
+		public readonly int VerticalSpeed;
+
+		/// <summary>
+		/// Aircraft type
+		/// </summary>
+		public readonly EAircraftType Type;
+
+		/// <summary>
+		/// Code and name of departure airport
+		/// </summary>
+		public readonly string Departure;
+
+		/// <summary>
+		/// Code and name of destination airport
+		/// </summary>
+		public readonly string Destination;
+
+		/// <summary>
+		/// If the aircraft is grounded
+		/// </summary>
+		public readonly bool Grounded;
+
+		/// <summary>
+		/// Call sign of the aircraft
+		/// </summary>
+		public readonly string CallSign;
+
+		/// <summary>
+		/// If the aircraft has a pic
+		/// </summary>
+		public readonly bool HasPicture;
+
+		/// <summary>
+		/// Number of flights recorded
+		/// </summary>
+		public readonly int FlightsCount;
+
+		/// <summary>
+		/// Constructs from a <see cref="FlightInfoResponse"/>
+		/// </summary>
+		/// <param name="info"></param>
+		public FlightInfo(FlightInfoResponse info)
+		{
+			Identifier         = info.Icao;
+			RegistrationNumber = info.Reg;
+			// TODO: This will probably fail
+			FirstSeen          = DateTime.Parse(info.Fseen);
+			Tracked            = TimeSpan.FromSeconds(info.Tsecs);
+			Latitude           = info.Lat;
+			Longitude          = info.Long;
+			// TODO: This might be incorrect
+			LastUpdate         = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)
+				.AddSeconds(info.PosTime);
+			Speed              = info.Spd;
+			SpeedType          = (ESpeedType) info.SpdTyp;
+			Angle              = info.Trak;
+			Model              = info.Type;
+			ModelDescription   = info.Mdl;
+			Manufacturer       = info.Man;
+			Year               = info.Year;
+			Operator           = info.Op;
+			VerticalSpeed      = info.Vsi;
+			Type               = (EAircraftType) info.Species;
+			Departure          = info.From;
+			Destination        = info.To;
+			Grounded           = info.Gnd;
+			CallSign           = info.Call;
+			HasPicture         = info.HasPic;
+			FlightsCount       = info.FlightsCount;
+		}
+	}
+}
---
+++ Data/FlightInfoResponse.cs
@@ -0,0 +1,123 @@
+﻿namespace ProjectFlight.Data
+{
+	/// <summary>
+	/// Represents the response from the flight tracking API
+	/// </summary>
+	public struct FlightInfoResponse
+	{
+		/// <summary>
+		/// Identifier broadcast by the aircraft
+		/// </summary>
+		public readonly string Icao;
+
+		/// <summary>
+		/// Aircraft registration number
+		/// </summary>
+		public readonly string Reg;
+
+		/// <summary>
+		/// First seen
+		/// </summary>
+		public readonly string Fseen;
+
+		/// <summary>
+		/// Seconds the aircraft has been tracked for
+		/// </summary>
+		public readonly int Tsecs;
+
+		/// <summary>
+		/// Latitude over the ground
+		/// </summary>
+		public readonly float Lat;
+
+		/// <summary>
+		/// Longitude over the ground
+		/// </summary>
+		public readonly float Long;
+
+		/// <summary>
+		/// Time position was last reported
+		/// </summary>
+		public readonly int PosTime;
+
+		/// <summary>
+		/// Speed in knots
+		/// </summary>
+		public readonly float Spd;
+
+		/// <summary>
+		/// Speed type
+		/// </summary>
+		public readonly int SpdTyp;
+
+		/// <summary>
+		/// Angle clockwise in degrees
+		/// </summary>
+		public readonly float Trak;
+
+		/// <summary>
+		/// Aircraft's model
+		/// </summary>
+		public readonly string Type;
+
+		/// <summary>
+		/// Description of aircraft's model
+		/// </summary>
+		public readonly string Mdl;
+
+		/// <summary>
+		/// Manufacturer's name
+		/// </summary>
+		public readonly string Man;
+
+		/// <summary>
+		/// Year manufactured
+		/// </summary>
+		public readonly int Year;
+
+		/// <summary>
+		/// Name of aircraft's operator
+		/// </summary>
+		public readonly string Op;
+
+		/// <summary>
+		/// Vertical speed in feet per minute
+		/// </summary>
+		public readonly int Vsi;
+
+		/// <summary>
+		/// Aircraft type
+		/// </summary>
+		public readonly int Species;
+
+		/// <summary>
+		/// Code and name of departure airport
+		/// </summary>
+		public readonly string From;
+
+		/// <summary>
+		/// Code and name of destination airport
+		/// </summary>
+		public readonly string To;
+
+		/// <summary>
+		/// If the aircraft is grounded
+		/// </summary>
+		public readonly bool Gnd;
+
+		/// <summary>
+		/// Call sign of the aircraft
+		/// </summary>
+		public readonly string Call;
+
+		/// <summary>
+		/// If the aircraft has a pic
+		/// </summary>
+		public readonly bool HasPic;
+
+		/// <summary>
+		/// Number of flights recorded
+		/// </summary>
+		public readonly int FlightsCount;
+	}
+}
