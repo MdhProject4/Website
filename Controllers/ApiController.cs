4048a6b1086f2c37822e1447caf628adb7169586
kraxarn <emil202821@hotmail.com>
2018-10-14 16:03:55 +0200

    Add search query option to GetFlights

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -14,8 +14,9 @@ namespace ProjectFlight.Controllers
 	    /// <param name="depDest">Only show flights who have a known departure and destination</param>
 	    /// <param name="inAir">Only show flights who are currently in the air</param>
 	    /// <param name="today">Only show flights that was added in the last 24 hours</param>
+	    /// <param name="query">Only show flights matching the query for departure or destination location</param>
 	    /// <returns></returns>
-	    public IActionResult GetFlights(bool depDest = false, bool inAir = false, bool today = false)
+	    public IActionResult GetFlights(bool depDest = false, bool inAir = false, bool today = false, string query = null)
         {
 			// First get all values
 	        IEnumerable<FlightInfo> flights;
@@ -30,6 +31,16 @@ namespace ProjectFlight.Controllers
 		        flights = flights.Where(f => f.Grounded == false);
 	        if (today)
 		        flights = flights.Where(f => f.Tracked != TimeSpan.Parse("23:59:59.9999999"));
+
+	        if (query != null)
+	        {
+				// Make query all lower case
+		        query = query.ToLower();
+
+				// Search for any match
+		        flights = flights.Where(f =>
+			        (f.Departure?.ToLower().Contains(query) ?? false) || (f.Destination?.ToLower().Contains(query) ?? false));
+	        }
 
 	        return new JsonResult(flights);
         }
