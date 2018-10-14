5d2e818f6f0c4e79d882659c5b57499f3d276b4d
kraxarn <emil202821@hotmail.com>
2018-10-14 12:25:08 +0200

    Add city and country to FlightInfo

--- Data/FlightInfo.cs
+++ Data/FlightInfo.cs
@@ -112,16 +112,75 @@ namespace ProjectFlight.Data
 		public EAircraftType Type { get; set; }
 
 		/// <summary>
+		/// Internal enum used for <see cref="GetInfoFromLocation"/>
+		/// </summary>
+		private enum LocationInfo
+		{
+			City,
+			Country
+		}
+
+		/// <summary>
+		/// Internally used to format city and country from location string (<see cref="Departure"/> or <see cref="Destination"/>)
+		/// </summary>
+		/// <param name="info"></param>
+		/// <param name="type"></param>
+		/// <returns></returns>
+		private static string GetInfoFromLocation(string info, LocationInfo type)
+		{
+			// To avoid crashing
+			if (info == null)
+				return null;
+
+			var i = info.Split(',');
+
+			/*
+			 * If length is 2: <code> <city>, <country>
+			 * If length is 3: <code> <airport>, <city>, <country>
+			 */
+
+			switch (i.Length)
+			{
+				// If it's not city, assume country
+				case 2: return type == LocationInfo.City ? i[0].Substring(4).Trim() : i[1].Trim();
+				case 3: return type == LocationInfo.City ? i[1].Trim()              : i[2].Trim();
+
+				// If not 2 or 3, something is wrong
+				default: return null;
+			}
+		}
+
+		/// <summary>
 		/// Code and name of departure airport
 		/// </summary>
 		[MaxLength(96)]
 		public string Departure { get; set; }
 
 		/// <summary>
+		/// Departure city. Null if <see cref="Departure"/> is null
+		/// </summary>
+		public string DepartureCity => GetInfoFromLocation(Departure, LocationInfo.City);
+
+		/// <summary>
+		/// Departure city. Null if <see cref="Departure"/> is null
+		/// </summary>
+		public string DepartureCountry => GetInfoFromLocation(Departure, LocationInfo.Country);
+
+		/// <summary>
 		/// Code and name of destination airport
 		/// </summary>
 		[MaxLength(96)]
 		public string Destination { get; set; }
+
+		/// <summary>
+		/// Departure city. Null if <see cref="Destination"/> is null
+		/// </summary>
+		public string DestinationCity => GetInfoFromLocation(Destination, LocationInfo.City);
+
+		/// <summary>
+		/// Departure city. Null if <see cref="Destination"/> is null
+		/// </summary>
+		public string DestinationCountry => GetInfoFromLocation(Destination, LocationInfo.Country);
 
 		/// <summary>
 		/// If the aircraft is grounded
