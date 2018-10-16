4ad92bd4ef7be28b97bfe2a7be09330004479275
kraxarn <emil202821@hotmail.com>
2018-10-16 22:16:05 +0200

    Clean up code

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,42 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using ProjectFlight.Data;
+using System;
 
 namespace ProjectFlight.Controllers
 {
-    public class UserController : Controller
+	public class UserController : Controller
     {
         public IActionResult Login (string username, string password)
         {
+			// TODO
             throw new NotImplementedException();
         }
 
-        public IActionResult Register(string username, string password, string email, bool isPremium)
+        public IActionResult Register(string username, string password, string email)
         {
-            int num;
-            User user = new User();
-            user.userName = username;
-            user.password = password;
-            user.email = email;
-            user.isPremium = false;
-            using (var context = new ApplicationDbContext())
+			// User we're adding
+	        var user = new User
+	        {
+		        userName  = username,
+		        password  = password,
+		        email     = email,
+		        isPremium = false
+	        };
+
+			// Temporary variable with number of rows affected
+	        int num;
+
+			// Add user to database
+	        using (var context = new ApplicationDbContext())
             {
                 context.User.Add(user);
                 num=context.SaveChanges();
             }
+
+			// Return json result of error
             return new JsonResult(new
             {
                 error = num == 0
             });
-           
-        }
-
-        public IActionResult Index()
-        {
-            return View();
         }
     }
 }
