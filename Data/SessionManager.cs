abad350c75cd4ccbdf30326632cc034c7abfcb5c
kraxarn <emil202821@hotmail.com>
2018-10-19 13:57:50 +0200

    Fix SessionManager

--- Data/SessionManager.cs
+++ Data/SessionManager.cs
@@ -1,30 +1,44 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
-using System.Security.Principal;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Http;
+using System;
+using System.Linq;
+using System.Security.Claims;
 
 namespace ProjectFlight.Data
 {
-	public abstract class SessionManager
+	/// <summary>
+	/// Session manager used to handle signed in users
+	/// </summary>
+	public class SessionManager
 	{
-        /// <summary>
-        /// Add a user to sessions and keep them logged in
-        /// </summary>
-        public async void Add(HttpContext context, string userId)
+		/// <summary>
+		/// HTTP Context used for handling stuff
+		/// </summary>
+		private readonly HttpContext context;
+
+		/// <summary>
+		/// Optional constructor to use the non-static methods
+		/// </summary>
+		/// <param name="context">Current HTTP context</param>
+		public SessionManager(HttpContext context) => this.context = context;
+
+		/// <summary>
+		/// Add a user to sessions and keep them logged in
+		/// </summary>
+		/// <param name="context">Current HTTP context</param>
+		/// <param name="username">The username of the user to add</param>
+		public static async void Add(HttpContext context, string username)
         {
             // If we're already signed in, ignore
-            if (context.User.Claims.Count() > 0) {
+            if (context.User.Claims.Any()) {
                 return;
             }
 
             // Claims
             var claims = new[]
             {
-                new Claim("Id", userId)
+                new Claim("Id", username)
             };
 
             // Add cookie
@@ -38,5 +52,36 @@ namespace ProjectFlight.Data
                 }
             );
         }
-    }
+
+		/// <summary>
+		/// Add a user to sessions and keep them logged in
+		/// </summary>
+		/// <param name="username">The username of the user to add</param>
+		public void Add(string username) => Add(context, username);
+
+		/// <summary>
+		/// Remove a user from sessions and log them out
+		/// </summary>
+		/// <param name="context">Current HTTP context</param>
+		public static async void Remove(HttpContext context) => await context.SignOutAsync();
+
+		/// <summary>
+		/// Remove a user from sessions and log them out
+		/// </summary>
+		public void Remove() => Remove(context);
+
+		/// <summary>
+		/// Get the current user
+		/// </summary>
+		/// <param name="context">Current HTTP context</param>
+		/// <returns>The username of the current user</returns>
+		public static string Get(HttpContext context) =>
+			context.User.Claims.FirstOrDefault(c => c.Type == "Id")?.Value;
+
+		/// <summary>
+		/// Get the current user
+		/// </summary>
+		/// <returns>The username of the current user</returns>
+		public string Get() => Get(context);
+	}
 }
