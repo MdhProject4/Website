d7590f08da25b7f2fc3f35e11b6ade4927f27510
kraxarn <emil202821@hotmail.com>
2018-10-19 14:13:00 +0200

    Make SessionManager static only

--- Data/SessionManager.cs
+++ Data/SessionManager.cs
@@ -10,19 +10,8 @@ namespace ProjectFlight.Data
 	/// <summary>
 	/// Session manager used to handle signed in users
 	/// </summary>
-	public class SessionManager
+	public static class SessionManager
 	{
-		/// <summary>
-		/// HTTP Context used for handling stuff
-		/// </summary>
-		private readonly HttpContext context;
-
-		/// <summary>
-		/// Optional constructor to use the non-static methods
-		/// </summary>
-		/// <param name="context">Current HTTP context</param>
-		public SessionManager(HttpContext context) => this.context = context;
-
 		/// <summary>
 		/// Add a user to sessions and keep them logged in
 		/// </summary>
@@ -31,11 +20,10 @@ namespace ProjectFlight.Data
 		public static async void Add(HttpContext context, string username)
         {
             // If we're already signed in, ignore
-            if (context.User.Claims.Any()) {
-                return;
-            }
+	        if (context.User.Claims.Any())
+		        return;
 
-            // Claims
+	        // Claims
             var claims = new[]
             {
                 new Claim("Id", username)
@@ -54,21 +42,10 @@ namespace ProjectFlight.Data
         }
 
 		/// <summary>
-		/// Add a user to sessions and keep them logged in
-		/// </summary>
-		/// <param name="username">The username of the user to add</param>
-		public void Add(string username) => Add(context, username);
-
-		/// <summary>
 		/// Remove a user from sessions and log them out
 		/// </summary>
 		/// <param name="context">Current HTTP context</param>
 		public static async void Remove(HttpContext context) => await context.SignOutAsync();
-
-		/// <summary>
-		/// Remove a user from sessions and log them out
-		/// </summary>
-		public void Remove() => Remove(context);
 
 		/// <summary>
 		/// Get the current user
@@ -77,11 +54,5 @@ namespace ProjectFlight.Data
 		/// <returns>The username of the current user</returns>
 		public static string Get(HttpContext context) =>
 			context.User.Claims.FirstOrDefault(c => c.Type == "Id")?.Value;
-
-		/// <summary>
-		/// Get the current user
-		/// </summary>
-		/// <returns>The username of the current user</returns>
-		public string Get() => Get(context);
 	}
 }
