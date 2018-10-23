b7863a49f02cb0e3b8bee24ddbd460c903cdb86c
kraxarn <emil202821@hotmail.com>
2018-10-23 17:40:07 +0200

    Initial notification

---
+++ Data/FlightNotification.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjectFlight.Data
+{
+	public class FlightNotification
+	{
+		/// <summary>
+		/// Id of the notification used by the database
+		/// </summary>
+		[Key]
+		[MaxLength(16)]
+		public string Id { get; set; }
+
+		/// <summary>
+		/// Username of the user who should be notified
+		/// </summary>
+		[MaxLength(64)]
+		public string Username { get; set; }
+
+		/// <summary>
+		/// ID of the flight
+		/// </summary>
+		[MaxLength(8)]
+		public string FlightId { get; set; }
+
+		/// <summary>
+		/// If the user has already been notified
+		/// </summary>
+		public bool Notified { get; set; }
+	}
+}
