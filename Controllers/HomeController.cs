38a29eaf245dcacc943587fb293c5a3d54060d79
kraxarn <emil202821@hotmail.com>
2018-10-24 15:57:10 +0200

    Move GetCountries to HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using ProjectFlight.Data;
 
 namespace ProjectFlight.Controllers
@@ -7,7 +9,30 @@ namespace ProjectFlight.Controllers
 	{
 		private ApplicationDbContext context;
 
-	    public HomeController(ApplicationDbContext context) => 
+		/// <summary>
+		/// Get all known countries
+		/// </summary>
+		/// <returns>List with all countries</returns>
+		public static IEnumerable<string> GetCountries()
+		{
+			// List to return later with all values
+			var all = new List<string>();
+
+			// Get values from db and add to list
+			using (var context = new ApplicationDbContext())
+			{
+				var departures   = from f in context.FlightInfos where f.Departure   != null select f.DepartureCountry;
+				var destinations = from f in context.FlightInfos where f.Destination != null select f.DestinationCountry;
+
+				all.AddRange(departures);
+				all.AddRange(destinations);
+			}
+
+			// Remove duplicates and sort them
+			return all.Distinct().OrderBy(f => f);
+		}
+
+		public HomeController(ApplicationDbContext context) => 
 		    this.context = context;
 
 		public IActionResult Index() => 
